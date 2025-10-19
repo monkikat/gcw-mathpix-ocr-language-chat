35c9e7c4c6ea7002106c0a7758b875758ca9dbfc
Noha Haneen <nohahaneensi@gmail.com>
2025-10-18 23:08:27 -0500

    implemented google gemini and created chat mode

--- gcw-ocr-language-chat/app/api/gemini-ai-model/route.ts
+++ gcw-ocr-language-chat/app/api/gemini-ai-model/route.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API || "");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash-001",
   tools: [
